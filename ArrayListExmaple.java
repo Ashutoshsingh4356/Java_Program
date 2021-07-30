897fc16635563c1fd19f738908d421d460c15f09
Ashutoshsingh4356 <ashutoshsingh4356@gmail.com>
2021-07-30 12:50:32 +0530

    mesaage

---
+++ ArrayListExmaple.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class ArrayListExmaple 
+
+{
+public static void main(String[] args)
+{
+	
+	
+	ArrayList list= new ArrayList();
+	list.add(10);
+	list.add(30.5);
+	list.add("ashutosh");
+	
+	System.out.println(list);
+	
+	
+//	
+//	ArrayList list<integer>= new ArrayList<integer>();
+//	list.add(10);
+//	list.add(30);                                              //when we have to use same type of dataype
+//	
+//	
+//	System.out.println(list);
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+}
